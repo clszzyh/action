2fb44b76723eded2e59f7fcfe510cb1c132c2e77
clszzyh <clszzyh@gmail.com>
2021-01-05 11:57:10 +0800

    initial
    Signed-off-by: clszzyh <clszzyh@gmail.com>

---
+++ lib/action.ex
@@ -0,0 +1,18 @@
+defmodule Action do
+  @moduledoc """
+  Documentation for `Action`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Action.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
