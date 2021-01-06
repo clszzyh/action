35782b3c34418577596f2166a4f2f5d660683fa0
clszzyh <clszzyh@gmail.com>
2021-01-06 16:53:02 +0800

    ok
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- lib/action/github.ex
+++ lib/action/github.ex
@@ -17,23 +17,29 @@ defmodule Action.Github do
   @enforce_keys [:client, :sha, :repository_name, :repository_owner, :event_name, :event]
   defstruct @enforce_keys
 
-  @spec init(binary() | nil) :: t()
+  @spec init(binary() | nil) :: {:ok, t()} | {:error, binary() | atom()}
   def init(arg \\ nil)
 
   def init(binary) when is_binary(binary) do
     %{token: token, repository: repository} = data = Jason.decode!(binary, keys: :atoms)
     [_, repository_name | []] = String.split(repository, "/")
 
-    struct(
-      __MODULE__,
-      Map.merge(data, %{
-        repository_name: repository_name,
-        client: Client.new(%{access_token: token})
-      })
-    )
+    {:ok,
+     struct(
+       __MODULE__,
+       Map.merge(data, %{
+         repository_name: repository_name,
+         client: Client.new(%{access_token: token})
+       })
+     )}
   end
 
   def init(nil) do
-    init(Api.fetch_input!("GITHUB"))
+    "GITHUB"
+    |> Api.get_input()
+    |> case do
+      nil -> {:error, "Not found `GITHUB` env"}
+      env -> init(env)
+    end
   end
 end
